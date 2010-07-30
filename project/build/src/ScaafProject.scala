10b6a332f89a07222c71c3d0b00d45def52a6286
Owen Fraser-Green <owen@fraser-green.com>
2010-07-30 15:27:38 +0200

    Bumped to sbinary 2.8.0

--- project/build/src/ScaafProject.scala
+++ project/build/src/ScaafProject.scala
@@ -4,7 +4,7 @@ import de.element34.sbteclipsify._
 class ScaliProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   val rabbitmq = "com.rabbitmq" % "amqp-client" % "1.7.2"
   val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.6.0"
-  val sbinary = "org.scala-tools.sbinary" % "sbinary_2.8.0.RC7" % "0.3.1-SNAPSHOT"
+  val sbinary = "org.scala-tools.sbinary" % "sbinary_2.8.0" % "0.3.1-SNAPSHOT"
   
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
   val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test"
